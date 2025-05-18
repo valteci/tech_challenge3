baf8029fcfb3fa12d46f3e3aa71f67ac42e91196
valteci <valtecijunior@gmail.com>
2025-05-18 12:29:41 -0300

    engenharia de feature - gols marcados

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from utils import Season
 
-class Model:
+class Preprocessing:
 
     DATA_PATH = './cleaned_data'
 
@@ -12,7 +12,7 @@ class Model:
         season = Season(19, 20)
 
         while season.next():
-            file_name = f'{Model.DATA_PATH}/{season.date}.csv'
+            file_name = f'{Preprocessing.DATA_PATH}/{season.date}.csv'
             df = pd.read_csv(file_name, sep=',')
             self._datas.append(df)
 
@@ -29,7 +29,55 @@ class Model:
         self._datas = value
 
 
+    def add_total_goals(self, year_index: int = 0) -> None:
+        """
+        Para o DataFrame em self._datas[year_index], adiciona:
+         - TotalHomeGoals: total de gols que o time mandante fez antes deste jogo
+         - TotalAwayGoals: total de gols que o time visitante fez antes deste jogo
+        """
+        df = self._datas[year_index].copy()
 
-model = Model()
+        # 1) Monta visão “long” com event_id para garantir ordem cronológica
+        df_home = df[['HomeTeam','FTHG']].rename(columns={'HomeTeam':'Team','FTHG':'Goals'}).copy()
+        df_home['match_id'] = df.index
+        df_home['side'] = 'home'
+        df_home['event_id'] = df.index * 2
+
+        df_away = df[['AwayTeam','FTAG']].rename(columns={'AwayTeam':'Team','FTAG':'Goals'}).copy()
+        df_away['match_id'] = df.index
+        df_away['side'] = 'away'
+        df_away['event_id'] = df.index * 2 + 1
+
+        df_long = pd.concat([df_home, df_away], ignore_index=True)
+        df_long = df_long.sort_values('event_id')  # garante ordem: jogo0-home, jogo0-away, jogo1-home, ...
+
+        # 2) Cálculo cumulativo por time, incluindo o evento atual, depois subtrai pra ficar só com anteriores
+        df_long['CumSum'] = df_long.groupby('Team')['Goals'].cumsum()
+        df_long['PrevGoals'] = df_long['CumSum'] - df_long['Goals']
+
+        # 3) Pivot pra voltar a wide com match_id como índice
+        pivot = df_long.pivot(index='match_id', columns='side', values='PrevGoals')
+
+        # 4) Preenche (0 onde não existir) e atribui ao DataFrame original
+        df['TotalHomeGoals']  = pivot['home'].fillna(0).astype(int).values
+        df['TotalAwayGoals']  = pivot['away'].fillna(0).astype(int).values
+
+        self._datas[year_index] = df
+
+
+
+
+model = Preprocessing()
 model._load_data()
-print(model.datas[0].describe())
+
+
+print(model.datas[0].head())
+
+print('\n========\n')
+
+# Aplica o método ao primeiro ano
+model.add_total_goals(0)
+
+# Depois: têm as colunas TotalHomeGoals e TotalAwayGoals
+print(model.datas[0])
+model.datas[0].to_csv('teste.csv', sep=',', index=False)
